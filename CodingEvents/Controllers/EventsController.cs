652073e7fc7462f39a030960bdffcd15b9f9a80c
Brigid <brigidbook@gmail.com>
2022-10-08 18:42:33 -0500

    did the assignment

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -1,20 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> events = new Dictionary<string, string>
-        {
-            { "Halloween party", "Come eat candy and carve pumpkins!" },
-            {"Pi day", "Fun math activities on March 14" },
-            {"Art fair", "See a variety of art from many artists." }
-        };
-        
+
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = events;
+            ViewBag.events = EventData.GetAll();
             return View();
         }
 
@@ -26,9 +23,47 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("Events/Add")]
-        public IActionResult NewEvent(string eventName, string eventDescription)
+        public IActionResult NewEvent(Event newEvent)
         {
-            events.Add(eventName, eventDescription);
+            EventData.Add(newEvent);
+            return Redirect("/Events");
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
+
+        [HttpGet]
+        [Route("Events/Edit/{eventId}")]
+        public IActionResult Edit(int eventId)
+        {
+            Event selected = EventData.GetById(eventId);
+            ViewBag.selectedEvent = selected;
+            ViewBag.title = $"Edit Event {selected.Name}(id = {eventId})";
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            Event selected = EventData.GetById(eventId);
+            selected.Name = name;
+            selected.Description = description;
             return Redirect("/Events");
         }
     }
---
+++ CodingEvents/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿namespace CodingEvents.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int Id { get; }
+        static private int nextId = 1;
+
+        public Event()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
